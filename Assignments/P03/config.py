9dcb6604a2e70898078c70f4fa5097353fd77ae6
Shaun Partridge <spartridgeb15@gmail.com>
2020-05-14 19:17:32 -0500

    update

--- Assignments/P03/config.py
+++ Assignments/P03/config.py
@@ -2,14 +2,25 @@ class Config:
     width = 1024
     height = 768
     sprite_images = {
-        "blue" : "./images/pac_blue_30x30.png",
-        "light_blue" : "./images/pac_light_blue_30x30.png",
         "red" : "./images/pac_red_30x30.png",
-        "white" : "./images/pac_white_30x30.png",
         "yellow" : "./images/pac_yellow_30x30.png",
-        "orange" : "./images/pac_orange_30x30.png",
         "green" : "./images/pac_green_30x30.png"
     }
+    social_distancing = False
+    infection_radius = 10
+    infection_rate = .20
+    population_count = 16
+    sprite_width = 15
+    sprite_height = 15
+    sprite_speed = 2
+Config.contacts = 5
+Config.colors = ["yellow","red","green"]
+Config.states = ["susceptible","infected","recovered"]
+Config.width = 400
+Config.height = 400
+Config.pid = 1
+Config.loops = 0
+Config.days = 0
 
 if __name__=='__main__':
     print("I'm testing config!")
